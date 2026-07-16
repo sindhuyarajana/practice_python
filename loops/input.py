432aed9321410b37d8088aaa1c632bab5acd45b9
sindhuyarajana <samsindhuy123@gmail.com>
2026-07-16 23:01:41 +0530

    sum

--- loops/input.py
+++ loops/input.py
@@ -2,6 +2,13 @@
 # for i in range(1,11):
 #     print(f"{number} x {i} = {number * i}")
 
+# number = int(input("enter a number: "))
+# for i in range(1,number + 1):
+#     print(i)
+
+total = 0
 number = int(input("enter a number: "))
-for i in range(1,number + 1):
-    print(i)
+for i in range(number + 1):
+    total += i
+
+print(total)
