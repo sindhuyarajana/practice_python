75fe0b9bac9096a49d338eac5fa51784155020c4
sindhuyarajana <samsindhuy123@gmail.com>
2026-07-16 22:32:40 +0530

    number sequence

--- loops/input.py
+++ loops/input.py
@@ -1,3 +1,7 @@
-number = int(input("Enter a number: "))
-for i in range(1,11):
-    print(f"{number} x {i} = {number * i}")
+# number = int(input("Enter a number: "))
+# for i in range(1,11):
+#     print(f"{number} x {i} = {number * i}")
+
+number = int(input("enter a number: "))
+for i in range(1,number + 1):
+    print(i)
