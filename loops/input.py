3a1a1f3483ca86c87e605011656b8a152c3120c9
sindhuyarajana <samsindhuy123@gmail.com>
2026-07-16 23:11:01 +0530

    count

--- loops/input.py
+++ loops/input.py
@@ -6,9 +6,16 @@
 # for i in range(1,number + 1):
 #     print(i)
 
-total = 0
-number = int(input("enter a number: "))
-for i in range(number + 1):
-    total += i
+# total = 0
+# number = int(input("enter a number: "))
+# for i in range(number + 1):
+#     total += i
 
-print(total)
+# print(total)
+count = 0
+number = int(input("enter a number: "))
+for i in range(1, number + 1):
+    if i % 3 == 0:
+        count += 1
+
+print(count)
