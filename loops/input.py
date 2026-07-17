f26a5977b668e6f47ff022ba0f308a4891ffc6a4
sindhuyarajana <samsindhuy123@gmail.com>
2026-07-17 20:29:03 +0530

    patterns using python

--- loops/input.py
+++ loops/input.py
@@ -1,17 +1,17 @@
-# number = int(input("Enter a number: "))
-# for i in range(1,11):
-#     print(f"{number} x {i} = {number * i}")
+number = int(input("Enter a number: "))
+for i in range(1,11):
+    print(f"{number} x {i} = {number * i}")
 
-# number = int(input("enter a number: "))
-# for i in range(1,number + 1):
-#     print(i)
+number = int(input("enter a number: "))
+for i in range(1,number + 1):
+    print(i)
 
-# total = 0
-# number = int(input("enter a number: "))
-# for i in range(number + 1):
-#     total += i
+total = 0
+number = int(input("enter a number: "))
+for i in range(number + 1):
+    total += i
 
-# print(total)
+print(total)
 
 number = int(input("enter a number: "))
 for i in range(5, number + 1, 5):
