d420a1e22ae89795f71f4003350ee6abbe6dea47
sindhuyarajana <samsindhuy123@gmail.com>
2026-07-17 21:33:30 +0530

    patterns

--- loops/pattern.py
+++ loops/pattern.py
@@ -17,7 +17,12 @@
 
 # That's the inner loop (j).
 
-for i in range(1, 6):
-    for j in range(1, i+1):
+# for i in range(1, 6):
+#     for j in range(1, i+1):
+#         print(j, end="")
+#     print()
+
+for i in range(5, 0, -1):
+    for j in range(5, i - 1, -1):
         print(j, end="")
     print()
