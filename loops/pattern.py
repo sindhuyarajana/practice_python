65e991a8ff3aba07bf3dd5e2747996dc11f462ab
sindhuyarajana <samsindhuy123@gmail.com>
2026-07-21 19:06:12 +0530

    patterns

--- loops/pattern.py
+++ loops/pattern.py
@@ -53,7 +53,12 @@
 #         print("*", end="")
 #     print()
 
-for i in range(5):        
-    for j in range(5):    
-        print("*", end="")
+# for i in range(5):        
+#     for j in range(5):    
+#         print("*", end="")
+#     print()
+
+for i in range(1, 6):        
+    for j in range(1, 6):    
+        print(j, end="")
     print()
