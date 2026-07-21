2b5bc34f847b96c833c18dd0de10e521f52b737e
sindhuyarajana <samsindhuy123@gmail.com>
2026-07-21 19:26:40 +0530

    patterns

--- loops/pattern.py
+++ loops/pattern.py
@@ -63,7 +63,12 @@
 #         print(j, end="")
 #     print()
 
-for i in range(1, 6):
-    for j in range(0, 5):
-        print(chr(65 + j), end="")
+# for i in range(1, 6):
+#     for j in range(0, 5):
+#         print(chr(65 + j), end="")
+#     print()
+
+for i in range(1,6):
+    for j in range(i):
+        print(chr(64 + i),end="")
     print()
