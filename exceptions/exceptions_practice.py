6e121cc901166b023c51f85c76928058a9141b2e
sindhuyarajana <samsindhuy123@gmail.com>
2026-08-14 19:48:30 +0530

    handling IndexError

--- exceptions/exceptions_practice.py
+++ exceptions/exceptions_practice.py
@@ -3,11 +3,20 @@
 # except:
 #     print("Something went wrong")
 ""
+#Write a program that asks the user for a number and handles the error if they enter text instead of a number.
+# def main():
+#     try:
+#         x = int(input("Enter a number: "))
+#         print(x)
+#     except ValueError:
+#         print("Please enter a valid number.")
+
+# main()
+
 def main():
     try:
-        x = int(input("Enter a number: "))
-        print(x)
-    except ValueError:
-        print("Please enter a valid number.")
-
+        numbers = [10, 20, 30]
+        print(numbers[5])
+    except IndexError:
+        print("Give a Valid Index")
 main()
